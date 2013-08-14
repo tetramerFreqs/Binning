9948b77f21fa71cdae60cce5c85288cb8191d48b
sunitj <sunitj@umich.edu>
2013-08-14 10:38:28 -0400

    Aug 2013 Updates

--- getClassFasta.pl
+++ getClassFasta.pl
@@ -1,63 +1,49 @@
 #! /usr/bin/perl
 
-=head1 NAME
+=head1 Description
 
-	getClassFasta
-		This program takes a chopped fasta file, a names file, a class file and extracts the seqs for the contigs presents in your desired class"; 
+	This program takes a class file, a names file, a fasta file and extracts the seqs for the contigs presents in the desired class.
 
-=head1 USAGE
-	
-	perl getClassFasta.pl -cls <CLASS File> -names <NAMES File> -fasta <Split FASTA File> -c <CLASS NUMBER>;
+=head1 Usage
 
-=head1 OPTIONS
-	
-	-cls		Required	cls file produced by the tetramer script.
-	-fasta		Required	Chopped Fasta File produced by the tetramer script.
-	-names	Required	names file produced by the tetramer script.
-	-c	Required	Class number you wish to extract.
-	
-=head1 AUTHORS
+	perl getClassFasta.pl -cls <CLASS File> -names <NAMES File> -fasta <Split FASTA File> -num <CLASS NUMBER>
 
-	Sunit Jain, 2010 (sunitj [AT] umich [DOT] edu)
-	
-=head3 About the author
+=head2 Options
 
-	www [DOT] sunitjain [DOT] com
-	
-=head1 LICENSE & COPYRIGHT
+	-cls	:	the .cls file
+	-names	:	the .names file
+	-fasta	:	The Concatenated fasta file that was used with the tetramer script.
+	-num	:	the class number you're interested in.
 
-	This software is released into the public domain. To the extent 
-	possible under law, all copyright and related or neighboring
-	rights are waived and permission is explicitly and irrevocably
-	granted to copy, modify, adapt and distribute this software
-	in any way you choose. This program is distributed in the hope
-	that it will be useful, but WITHOUT ANY WARRANTY; without even 
-	the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-	PARTICULAR PURPOSE.
+=head1 Questions/Comments/Suggestions/Accolades/Beer
+
+	Sunit Jain, sunitj [AT] umich [DOT] edu
 
 =cut
 
+
 use strict;
 use Getopt::Long;
+use Pod::Usage;
 
-my ($classFile, $namesFile,$fastaFile, $classNum);
-my $version=$0." v1.0.0";
+my ($class, $names, $fasta, $classNum);
+my $version="0.0.02";
 
 GetOptions(
-	'fasta=s'=> \$fastaFile,
-	'cls|class=s'=>\$classFile,
-	'name|names=s'=>\$namesFile,
-	'c=i'=>\$classNum,
-	'v|version'=>sub{print $version."\n"; exit;},
-	'h|help'=> sub{system('perldoc', $0); exit;},
+	"cls=s"=>\$class,
+	"names=s"=>\$names,
+	"fasta=s"=>\$fasta,
+	"num=i"=>\$classNum,
+	"help"=>sub{system("perldoc", $0); exit;},
 );
 
-if ((!$classFile) || (!$namesFile) || (!$fastaFile) || (!$classNum)){system('perldoc', $0); exit;}
+print "getClassFasta v$version\n";
 
+die if (! $class or ! $names or ! $fasta or ! $classNum);
 
 # Parse *.cls file to get SeqID for all Seqs in the desired class
 my %clsHash;
-open ( CLS, $classFile) || die "ERROR: $classFile.\n".$!;
+open ( CLS, $class) || die "ERROR: $class.\n".$!;
 	while (my $line=<CLS>){
 		chomp($line);
 		unless ($line=~ /^%/){
@@ -71,11 +57,11 @@ close CLS;
 
 # Parse the *.names file to id the seq names in the fasta file using the classHash from above.
 my %seqNames;
-open (NAMES, $namesFile) || die "ERROR: $namesFile.\n".$!;
+open (NAMES, $names) || die "ERROR: $names.\n".$!;
 	while (my $line=<NAMES>){
 		chomp($line);
 		unless ($line =~ /^%/){
-			my ($seqNum, $seqFastaName, $seqName)=split(/\t/, $line);
+			my ($seqNum, $seqName, $seqFastaName)=split(/\t/, $line);
 			if ($clsHash{$seqNum}){
 				$seqNames{$seqFastaName}=$seqName; # %seqNames {Name of Seq part of the contig => Name of the whole contig}
 			}
@@ -84,10 +70,10 @@ open (NAMES, $namesFile) || die "ERROR: $namesFile.\n".$!;
 close NAMES;
 undef %clsHash;
 
-# Parse the split fasta file to get the desired sequences, using the seqNames hash from above.
+# Parse the fasta file to get the desired sequences, using the seqNames hash from above.
 my $outFile=$classNum.".fasta";
 open (OUT,">".$outFile )|| die "ERROR: $outFile.\n".$!;
-open (FASTA, $fastaFile) || die "ERROR: $fastaFile.\n".$!;
+open (FASTA, $fasta) || die "ERROR: $fasta.\n".$!;
 	$/= ">";
 	while (my $line = <FASTA>) {
 		chomp $line;
@@ -101,3 +87,4 @@ open (FASTA, $fastaFile) || die "ERROR: $fastaFile.\n".$!;
 	$/= "\n";
 close FASTA;
 close OUT;
+
